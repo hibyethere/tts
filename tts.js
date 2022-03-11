46828532163a0623382417b7dd9ffe628c2f0efd
hibyethere <realizeyou0306@gmail.com>
2022-03-11 18:46:18 +0900

    change range

--- tts.js
+++ tts.js
@@ -95,8 +95,8 @@ function displayResultFromChat(result, msg, usecoloredchat = true) {
  * @param {Function} callback 실행 결과 콜백
  */
 function randomVoice(username, callback) {
-    let randomSpeed = Math.random() * (1.4 - 1.0) + 1.0;
-    let randomPitch = Math.random() * (1.2 - 0.9) + 0.9;
+    let randomSpeed = Math.random() * (1.4 - 0.8) + 0.8;
+    let randomPitch = Math.random() * (1.4 - 0.8) + 0.8;
     if (username != "") {
         localStorage.setItem(username + ":speed", randomSpeed.toFixed(1));
         localStorage.setItem(username + ":pitch", randomPitch.toFixed(1));
